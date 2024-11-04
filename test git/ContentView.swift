7337b6e145d05e100fde64378a2f9f86188fe896
supriya <hm7020@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ test git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test git
+//
+//  Created by Palle Supriya on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test git
+//
+//  Created by Palle Supriya on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
